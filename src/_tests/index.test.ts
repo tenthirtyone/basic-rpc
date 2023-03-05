9d6b5a58f7f44e7ff33840bc0e6534d8b7e4ab7e
tenthirtyone <alexsherbuck@gmail.com>
2023-03-05 14:05:21 -0600

    mine genesis block

--- src/_tests/index.test.ts
+++ src/_tests/index.test.ts
@@ -2,11 +2,14 @@ import assert from "assert";
 import BasicRPC from "../";
 
 describe("Basic RPC", () => {
-  let basicrpc;
+  let basicrpc: BasicRPC;
 
   beforeEach(() => {
     basicrpc = new BasicRPC();
   });
 
-  it("exists", () => {});
+  it("exists", async () => {
+    await basicrpc.start();
+    console.log(await basicrpc.getBlock(0));
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -19,10 +19,17 @@ export default class BasicRPC {
   constructor(options?: BasicRPCOptions) {
     this._options = mergeDeep(BasicRPC.DEFAULTS, options);
     this._common = new Common({ ...this._options.common });
+    this.start();
   }
 
   async start() {
     this._blockchain = await Blockchain.create({ common: this._common, db });
+  }
+
+  async getBlock(number: number) {
+    if (!this._blockchain) return number;
+
+    return await this._blockchain.getBlock(number);
   }
 
   static get DEFAULTS(): BasicRPCOptions {
